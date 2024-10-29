833700e054383ae86f475ace3a4ef1ca385362f2
Claus Guttesen <kometen@gmail.com>
2024-10-29 22:12:44 +0100

    Use a struct to hold the fields retrieved from Azure Key Vault

---
+++ src/database_config.rs
@@ -0,0 +1,63 @@
+use crate::Vault;
+use anyhow::Result;
+use serde::Serialize;
+
+#[derive(Serialize, Default)]
+pub struct DatabaseConfig {
+    host: String,
+    user: String,
+    name: String,
+    pwd: String,
+    domain: String,
+}
+
+impl DatabaseConfig {
+    pub fn from_vault(vault: &Vault) -> Result<Self> {
+        Ok(Self {
+            host: vault.get_required("db-host")?,
+            user: vault.get_required("db-user")?,
+            name: vault.get_required("db-name")?,
+            pwd: vault.get_required("db-pwd")?,
+            domain: vault.get_required("db-domain")?,
+        })
+    }
+
+    pub fn connection_string(&self) -> String {
+        format!(
+            "postgres://{}@{}.{}/{}",
+            self.user, self.host, self.domain, self.name
+        )
+    }
+
+    pub fn db_keys() -> Vec<String> {
+        keys()
+    }
+
+    pub fn domain(&self) -> String {
+        self.domain.clone()
+    }
+
+    pub fn host(&self) -> String {
+        self.host.clone()
+    }
+
+    pub fn password(&self) -> String {
+        self.pwd.clone()
+    }
+}
+
+pub fn keys() -> Vec<String> {
+    let fields: Vec<String> = serde_json::Value::Object(
+        serde_json::to_value(DatabaseConfig::default())
+            .unwrap()
+            .as_object()
+            .unwrap()
+            .clone(),
+    )
+    .as_object()
+    .unwrap()
+    .keys()
+    .map(|k| format!("db-{}", k))
+    .collect();
+    fields
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 pub mod connect;
+pub mod database_config;
 pub mod dns;
 pub mod secret_manager;
 pub mod vault;
 
 pub use connect::connect::run_psql;
+pub use database_config::DatabaseConfig;
 pub use dns::dns::check_dns;
 pub use secret_manager::SecretManager;
 pub use vault::Vault;
